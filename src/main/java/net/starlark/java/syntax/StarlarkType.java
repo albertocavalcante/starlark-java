99cdbe4be6a47909f99729a62621acfb66f9e739
brandjon <brandjon@google.com>
2026-01-16 09:12:59 -0800

    Merge types/ package into syntax/
    There's very little logic that's self-contained to types without reference to AST elements (for statically evaluating and checking types). Let's simplify a bit by eliminating the types package.
    
    Work toward #28043.
    
    PiperOrigin-RevId: 857187700
    Change-Id: I0a6f9939e0782b58ac0b23ed160ae914bdbd2cce
    Bazel-Commit=a1a9148d1e3ea560c69b3e3c93b9c75c6c711a6d
    Upstream-Source=bazelbuild/bazel
    GitOrigin-RevId: a1a9148d1e3ea560c69b3e3c93b9c75c6c711a6d

---
+++ src/main/java/net/starlark/java/syntax/StarlarkType.java
@@ -0,0 +1,67 @@
+// Copyright 2025 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package net.starlark.java.syntax;
+
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+
+/**
+ * Base class for all Starlark types.
+ *
+ * <p>Starlark typing is an experimental feature under development. See the tracking issue:
+ * https://github.com/bazelbuild/bazel/issues/27370
+ */
+public abstract class StarlarkType {
+
+  /**
+   * Returns the list of supertypes of this type.
+   *
+   * <p>Preferred order is from the most specific to the least specific supertype. But if that is
+   * not possible, the order can be arbitrary.
+   */
+  // TODO: #27370 - Add getSubtypes(), with the semantics that the actual subtype relation is the
+  // union of these two methods.
+  public List<StarlarkType> getSupertypes() {
+    return ImmutableList.of();
+  }
+
+  /**
+   * Returns whether a value of type {@code t2} can be assigned to a value of type {@code t1}.
+   *
+   * <p>In gradual typing terms, {@code t2} must be a "consistent subtype of" {@code t1}. This means
+   * that there is a way to substitute zero or more occurrences of {@code Any} in both terms, such
+   * that {@code t2} becomes a subtype of {@code t1} in the ordinary sense.
+   *
+   * <p>The Python glossary uses the term "assignable [to/from]" for this relation, and
+   * "materialization" to refer to the process of substituting {@code Any}.
+   */
+  // TODO: #28043 - Add support for:
+  // - subtyping (list[int] <= Sequence[int])
+  // - covariance (Sequence[int] <= Sequence[object])
+  // - proper treatment of materializing Any (Sequence[int] <= Sequence[Any])
+  // - transitive application of all of the above, including across unions
+  public static boolean assignableFrom(StarlarkType t1, StarlarkType t2) {
+    if (t1.equals(Types.ANY) || t2.equals(Types.ANY)) {
+      return true;
+    }
+    if (t1.equals(Types.OBJECT)) {
+      return true;
+    }
+    if (t1.equals(t2)) {
+      return true;
+    }
+    return false;
+  }
+}
